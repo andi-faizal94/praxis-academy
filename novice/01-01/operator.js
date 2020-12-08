e0a04abf585c3b2f68fdbaa38da240c05762b51d
andi9475 <andifz9475@gmail.com>
2020-12-08 16:42:31 +0700

    menambahkan math.flor di file operator.js

---
+++ novice/01-01/operator.js
@@ -0,0 +1,26 @@
+var a = 20;
+var b = 10;
+
+const phy = 3.14;
+var r = 3;
+var luas = phy * r ** r;
+// addition
+console.log(a + b)
+    // substraction 
+console.log(a - b)
+    // multiline
+console.log(a * b)
+    // division
+console.log(a / b)
+    // modulus
+console.log(a % b)
+    // pangkat
+console.log(a ** b)
+
+// luas lingkaran
+
+console.log("luas lingkaran :", Math.floor(luas))
+
+
+console.log(a == b)
+console.log(a != b)
