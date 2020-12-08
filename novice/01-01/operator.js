e4b5f1aa5e91d39cfd53815531061cd30c765e80
andi9475 <andifz9475@gmail.com>
2020-12-08 16:24:05 +0700

    menambahkan luas lingkaran

--- novice/01-01/operator.js
+++ novice/01-01/operator.js
@@ -1,6 +1,9 @@
 var a = 20;
 var b = 10;
 
+const phy = 3.14;
+var r = 3;
+var luas = phy * r ** r;
 // addition
 console.log(a + b)
     // substraction 
@@ -13,6 +16,11 @@ console.log(a / b)
 console.log(a % b)
     // pangkat
 console.log(a ** b)
+
+// luas lingkaran
+
+console.log("luas lingkaran :", luas)
+
 
 console.log(a == b)
 console.log(a != b)
