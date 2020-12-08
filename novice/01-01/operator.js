13e9226d4f9f7ff5d8b1662839c9c961e2cfcab4
andi9475 <andifz9475@gmail.com>
2020-12-08 16:00:50 +0700

    menambahkan tugas dan folder novice

---
+++ novice/01-01/operator.js
@@ -0,0 +1,18 @@
+var a = 20;
+var b = 10;
+
+// addition
+console.log(a + b)
+    // substraction 
+console.log(a - b)
+    // multiline
+console.log(a * b)
+    // division
+console.log(a / b)
+    // modulus
+console.log(a % b)
+    // pangkat
+console.log(a ** b)
+
+console.log(a == b)
+console.log(a != b)
