521ec771965998f5422baad46d1cc805b8c05b38
andi-faizal94 <andifaizal647@gmail.com>
2020-12-15 09:13:17 +0700

    menambahkan file

--- novice/02-01/kasus/kasus.js
+++ novice/02-01/kasus/kasus.js
@@ -19,25 +19,30 @@ function cat(tiredness, hunger, lonliness, happiness) {
     this.dead = function() {
         return this.lonliness++;
     }
+    this.hungry = function(a) {
 
-    if (this.hunger <= 1) {
-        console.log('pows realy hungry');
-    } else {
-        console.log('pow enough')
-    };
-    if (this.walk <= 1) {
-        console.log('paw very happy');
-    } else {
-        console.log('paw is sad')
+        if (this.hunger >= a) {
+            return 'pows realy hungry';
+        } else {
+            return 'pow enough';
+        }
     }
 
+    this.walking = function(b) {
+        if (this.walk >= b) {
+            return 'paw very happy';
+        } else {
+            return 'paw is sad';
+        }
+    }
 
-    // this.getName = function() {
-    //     return "cat is" + " " + this.tiredness + " " + this.hunger + " " + this.lonliness + " " + this.happiness;
 }
-
 let cat1 = new cat(1, 1, 1, 1);
 cat1.feed();
 cat1.dead();
 cat1.walk();
 cat1.sleep();
+// let cat2 = new cat(1, 1, 1, 1)
+// cat1.hungry(6);
+console.log(cat1.hungry(6));
+console.log(cat1.walking(3));
