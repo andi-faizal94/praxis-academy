6aeccdf763ce363255748f47cb3a2ed8da3a69a5
andi-faizal94 <andifaizal647@gmail.com>
2020-12-24 15:23:12 +0700

    belajar dan latihan tentang lifecycle hooks dan router

---
+++ novice/03-03/latihan dan kasus/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+    <h1>Welcome in my page</h1>
+    <br />
+    <br />
+    <br />
+    <h6>Input Your Name</h6>
+    <tyr></tyr>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import tyr from "@/components/tyr.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld,
+    tyr
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: aqua;
+}
+</style>
---
+++ novice/03-03/latihan dan kasus/tyr.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1 v-text="name"></h1>
+    <p v-show="peace">hallo</p>
+
+    <input v-model="name" type="text" />
+    <button v-on:click="name=''">change</button>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      life: "life cycle",
+      // people: {
+      name: "",
+      age: 14,
+      place: "temanggung",
+      peace: true
+      // }
+    };
+  },
+  // beforeCreate() {
+  // alert(this.people.name + " " + " before created");
+  // },
+  created() {
+    alert(this.people.name + " " + " created");
+  },
+  // beforeMount() {
+  // alert(this.people.name + " " + " before mount");
+  // },
+  // mounted() {
+  // alert(this.people.name + " " + " mounted");
+  // },
+  // beforeUpdate() {
+  // alert(this.people.name + " " + " before updated");
+  // },
+  // updated() {
+  // alert(this.people.name + " " + " updated");
+  // },
+  beforeDestroy() {
+    alert(this.name + " " + " before destroyed");
+  },
+  destroyed() {
+    alert(this.name + " " + " destroyed");
+  }
+};
+</script>
